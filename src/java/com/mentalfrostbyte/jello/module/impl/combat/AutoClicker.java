84b642ca8855a94a111d06ac45db7d0e4fd852a0
Mark <86848614+MarkGG8181@users.noreply.github.com>
2024-10-18 22:00:27 +0300

    remap auto clicker & fix 4.6 reach

--- src/java/com/mentalfrostbyte/jello/module/impl/combat/AutoClicker.java
+++ src/java/com/mentalfrostbyte/jello/module/impl/combat/AutoClicker.java
@@ -5,37 +5,39 @@ import com.mentalfrostbyte.jello.event.impl.TickEvent;
 import com.mentalfrostbyte.jello.module.Module;
 import com.mentalfrostbyte.jello.module.ModuleCategory;
 import com.mentalfrostbyte.jello.util.MultiUtilities;
+import net.minecraft.entity.Entity;
 import net.minecraft.network.play.client.CUseEntityPacket;
 import net.minecraft.util.Hand;
 
 import java.util.Random;
 
 public class AutoClicker extends Module {
-    private int field23548;
+    private int tickCounter;
 
     public AutoClicker() {
         super(ModuleCategory.COMBAT, "AutoClicker", "Longpress your attack keybind to hit entities automaticly");
     }
 
     @EventTarget
-    private void method16244(TickEvent var1) {
+    private void onTick(TickEvent event) {
         if (this.isEnabled()) {
-            this.field23548++;
-            Random var4 = new Random();
-            int var5 = 2;
-            int var6 = 4;
-            int var7 = var4.nextInt(var6 - var5) + var5;
-            if (mc.gameSettings.keyBindAttack.isKeyDown() && this.field23548 >= var7) {
+            this.tickCounter++;
+
+            Random random = new Random();
+            int randomDelay = random.nextInt(4 - 2) + 2;
+
+            if (mc.gameSettings.keyBindAttack.isKeyDown() && this.tickCounter >= randomDelay) {
                 mc.player.swingArm(Hand.MAIN_HAND);
-                this.field23548 = 0;
-                if (MultiUtilities.method17711(mc.player.rotationYaw, mc.player.rotationPitch, 4.6F, 0.0) != null) {
-                    mc.getConnection()
-                            .sendPacket(
-                                    new CUseEntityPacket(
-                                            MultiUtilities.method17711(mc.player.rotationYaw, mc.player.rotationPitch, 4.6F, 0.0), mc.player.isSneaking()
-                                    )
-                            );
-                    mc.player.swingArm(Hand.MAIN_HAND);
+
+                this.tickCounter = 0;
+
+                if (MultiUtilities.getEntityFromRayTrace(mc.player.rotationYaw, mc.player.rotationPitch, 3.0F, 0.0) != null) {
+                    Entity target = MultiUtilities.getEntityFromRayTrace(mc.player.rotationYaw, mc.player.rotationPitch, 3.0F, 0.0);
+
+                    if (target != null) {
+                        mc.getConnection().sendPacket(new CUseEntityPacket(target, mc.player.isSneaking()));
+                        mc.player.swingArm(Hand.MAIN_HAND);
+                    }
                 }
             }
         }
