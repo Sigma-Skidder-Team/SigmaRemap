ff668cd142320baed385deefa83dd26f38b08378
coffee <185070888+ccfeeX@users.noreply.github.com>
2024-10-18 19:55:15 +0200

    Updated AutoSprint.java
    Made AutoSprint.java cleaner & easier to read.

--- src/java/com/mentalfrostbyte/jello/module/impl/player/AutoSprint.java
+++ src/java/com/mentalfrostbyte/jello/module/impl/player/AutoSprint.java
@@ -17,8 +17,8 @@ import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
 import net.minecraft.item.Items;
 
 public class AutoSprint extends Module {
-    private final double[] field23605 = new double[]{0.0, 0.0};
-    private boolean field23606;
+    private final double[] TrackMotion = new double[]{0.0, 0.0};
+    private boolean isSprinting;
 
     public AutoSprint() {
         super(ModuleCategory.PLAYER, "AutoSprint", "Sprints for you");
@@ -26,50 +26,50 @@ public class AutoSprint extends Module {
     }
 
     @EventTarget
-    public void method16343(TickEvent var1) {
+    public void TickEvent(TickEvent event) {
         if (this.isEnabled()) {
-            ModuleWithModuleSettings var4 = (ModuleWithModuleSettings) Client.getInstance().getModuleManager().getModuleByClass(BlockFly.class);
-            Module var5 = var4.parentModule;
-            if (var5 == null || !var5.isEnabled() || !(var5 instanceof BlockFlyAACMode) || var5.getBooleanValueFromSettingName("Haphe (AACAP)")) {
+            ModuleWithModuleSettings getModule = (ModuleWithModuleSettings) Client.getInstance().getModuleManager().getModuleByClass(BlockFly.class);
+            Module BlockFly = getModule.parentModule;
+            if (BlockFly == null || !BlockFly.isEnabled() || !(BlockFly instanceof BlockFlyAACMode) || BlockFly.getBooleanValueFromSettingName("Haphe (AACAP)")) {
                 mc.player.setSprinting(mc.player.moveForward > 0.0F && !((BlockFly) Client.getInstance().getModuleManager().getModuleByClass(BlockFly.class)).method16732());
             }
         }
     }
 
     @EventTarget
-    public void method16344(Class4423 var1) {
+    public void sigma(Class4423 event) {
         if (this.isEnabled()
                 && !(mc.player.moveForward <= 0.0F)
                 && (!mc.player.isHandActive() || mc.player.getActiveItemStack().getItem() != Items.BOW)
                 && !((BlockFly) Client.getInstance().getModuleManager().getModuleByClass(BlockFly.class)).method16732()) {
-            ModifiableAttributeInstance var4 = mc.player.getAttribute(Attributes.MOVEMENT_SPEED);
-            float var5 = (float) (
-                    (var4.getBaseValue() + 0.03F + (double) (0.015F * (float) MovementUtils.method37078())) / (double) mc.player.abilities.getWalkSpeed() + 1.0
+            ModifiableAttributeInstance getAttribute = mc.player.getAttribute(Attributes.MOVEMENT_SPEED);
+            float BlockFly = (float) (
+                    (getAttribute.getBaseValue() + 0.03F + (double) (0.015F * (float) MovementUtils.method37078())) / (double) mc.player.abilities.getWalkSpeed() + 1.0
             )
                     / 2.0F;
-            var1.field21557 = var5;
+            event.field21557 = BlockFly;
         }
     }
 
     @EventTarget
-    public void method16345(EventRayTraceResult var1) {
+    public void RayTraceEvent(EventRayTraceResult event) {
         if (this.isEnabled() && this.getBooleanValueFromSettingName("Keep Sprint")) {
-            if (!var1.isHovering()) {
-                if (this.field23605.length == 2) {
-                    double var4 = this.field23605[0] - mc.player.getMotion().x;
-                    double var6 = this.field23605[1] - mc.player.getMotion().z;
-                    if (var4 != 0.0 || var6 != 0.0) {
-                        mc.player.setMotion(this.field23605[0], mc.player.getMotion().y, this.field23605[1]);
+            if (!event.isHovering()) {
+                if (this.TrackMotion.length == 2) {
+                    double MotionX = this.TrackMotion[0] - mc.player.getMotion().x;
+                    double MotionZ = this.TrackMotion[1] - mc.player.getMotion().z;
+                    if (MotionX != 0.0 || MotionZ != 0.0) {
+                        mc.player.setMotion(this.TrackMotion[0], mc.player.getMotion().y, this.TrackMotion[1]);
                     }
-
-                    if (this.field23606 && !mc.player.isSprinting()) {
+                     // if the player should be sprinting but isnt start sprinting
+                    if (this.isSprinting && !mc.player.isSprinting()) {
                         mc.player.setSprinting(true);
                     }
                 }
             } else {
-                this.field23605[0] = mc.player.getMotion().x;
-                this.field23605[1] = mc.player.getMotion().z;
-                this.field23606 = mc.player.isSprinting();
+                this.TrackMotion[0] = mc.player.getMotion().x;
+                this.TrackMotion[1] = mc.player.getMotion().z;
+                this.isSprinting = mc.player.isSprinting();
             }
         }
     }
