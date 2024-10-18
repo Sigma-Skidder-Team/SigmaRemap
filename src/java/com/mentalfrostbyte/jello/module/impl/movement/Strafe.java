c350866acc8a88cedde6983e01dc76bb8052fe26
coffee <185070888+ccfeeX@users.noreply.github.com>
2024-10-18 22:16:54 +0200

    Remapped Strafe.java

--- src/java/com/mentalfrostbyte/jello/module/impl/movement/Strafe.java
+++ src/java/com/mentalfrostbyte/jello/module/impl/movement/Strafe.java
@@ -7,43 +7,41 @@ import com.mentalfrostbyte.jello.module.ModuleCategory;
 import lol.MovementUtils;
 
 public class Strafe extends Module {
-    public double field23936;
-    private double field23932;
-    private float field23933;
-    private float field23934;
-    private boolean field23935;
+    private double currentSpeed; 
+    private double lastSpeed;
 
     public Strafe() {
         super(ModuleCategory.MOVEMENT, "Strafe", "Strafe in mid air");
     }
 
     @EventTarget
-    public void method16817(EventMove var1) {
+    public void onMove(EventMove event) {
         if (this.isEnabled()) {
-            this.field23932 = MovementUtils.getSpeed();
-            float var4 = MovementUtils.lenientStrafe()[1];
-            float var5 = MovementUtils.lenientStrafe()[2];
-            float var6 = MovementUtils.method37086();
-            double var7 = Math.cos(Math.toRadians(var6));
-            double var9 = Math.sin(Math.toRadians(var6));
-            double var11 = Math.sqrt(var1.getX() * var1.getX() + var1.getZ() * var1.getZ());
+            lastSpeed = MovementUtils.getSpeed();
+            float strafeDirection = MovementUtils.lenientStrafe()[1];
+            float forwardDirection = MovementUtils.lenientStrafe()[2];
+            float playerAngle = MovementUtils.method37086();
+
+            double cosAngle = Math.cos(Math.toRadians(playerAngle));
+            double sinAngle = Math.sin(Math.toRadians(playerAngle));
+
+            double movementMagnitude = Math.sqrt(event.getX() * event.getX() + event.getZ() * event.getZ());
             if (!MovementUtils.isMoving()) {
-                var11 = 0.0;
+                movementMagnitude = 0.0;
             }
+            float smoothingFactor = 0.2F;
 
-            float var13 = 0.2F;
-            if (!(var11 > this.field23936 + 0.1F)) {
-                this.field23936 = var11;
-                if (var11 != 0.0) {
-                    var11 = Math.max(var11, MovementUtils.getSpeed());
+            if (!(movementMagnitude > currentSpeed + 0.1F)) {
+                currentSpeed = movementMagnitude;
+
+                if (movementMagnitude != 0.0) {
+                    movementMagnitude = Math.max(movementMagnitude, MovementUtils.getSpeed());
                 }
 
-                var1.setX(var1.getX() * (double) (1.0F - var13) + var11 * var7 * (double) var13);
-                var1.setZ(var1.getZ() * (double) (1.0F - var13) + var11 * var9 * (double) var13);
-                if (MovementUtils.isMoving()) {
-                }
+                event.setX(event.getX() * (1.0F - smoothingFactor) + movementMagnitude * cosAngle * smoothingFactor);
+                event.setZ(event.getZ() * (1.0F - smoothingFactor) + movementMagnitude * sinAngle * smoothingFactor);
             } else {
-                this.field23936 = var11;
+                currentSpeed = movementMagnitude;
             }
         }
     }
