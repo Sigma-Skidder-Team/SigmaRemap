805f8ed25be06c126eb0279da8643d4c03fa9ce5
coffee <185070888+ccfeeX@users.noreply.github.com>
2024-10-18 22:12:18 +0200

    Remapped UnfullPhase.java

--- src/java/com/mentalfrostbyte/jello/module/impl/movement/phase/UnfullPhase.java
+++ src/java/com/mentalfrostbyte/jello/module/impl/movement/phase/UnfullPhase.java
@@ -7,60 +7,59 @@ import com.mentalfrostbyte.jello.event.impl.EventPushBlock;
 import com.mentalfrostbyte.jello.module.Module;
 import com.mentalfrostbyte.jello.module.ModuleCategory;
 import com.mentalfrostbyte.jello.util.MultiUtilities;
-import mapped.*;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.block.StairsBlock;
 
 public class UnfullPhase extends Module {
-    private int field23843;
-    private double field23844;
+    private int currentYPosition;
+    private double yOffset;
 
     public UnfullPhase() {
         super(ModuleCategory.MOVEMENT, "Unfull", "Weird Hypixel VClip for some blocks");
     }
-
+    
     @EventTarget
-    private void method16680(EventWalkingUpdate var1) {
+    private void onWalkingUpdate(EventWalkingUpdate event) {
         if (this.isEnabled()) {
-            if ((int) mc.player.getPosY() == this.field23843 && !mc.player.isJumping) {
+            if ((int) mc.player.getPosY() == currentYPosition && !mc.player.isJumping) {
                 MultiUtilities.setPlayerYMotion(-2.0);
             }
 
-            if (mc.player.getPosY() > (double) this.field23843 && mc.player.isJumping && !mc.player.isSneaking()) {
-                this.field23843++;
+            if (mc.player.getPosY() > (double) currentYPosition && mc.player.isJumping && !mc.player.isSneaking()) {
+                currentYPosition++;
             }
 
             if (mc.player.getPosY() % 1.0 == 0.0 && mc.player.isSneaking()) {
-                this.field23843--;
+                currentYPosition--;
+            }
+        }
+    }
+    
+    @EventTarget
+    private void onBlockCollision(EventBlockCollision event) {
+        if (this.isEnabled()) {
+            if (event.getVoxelShape() != null &&
+                    (event.getBlockPos().getY() == currentYPosition - 1 ||
+                            event.getBlockPos().getY() == currentYPosition ||
+                            (event.getBlockPos().getY() == currentYPosition + 1 &&
+                                    mc.world.getBlockState(event.getBlockPos()).getBlock() instanceof SlabBlock) ||
+                            mc.world.getBlockState(event.getBlockPos()).getBlock() instanceof StairsBlock)) {
+
+                event.setBoxelShape(null);
             }
         }
     }
 
     @EventTarget
-    private void method16681(EventBlockCollision var1) {
+    private void onPushBlock(EventPushBlock event) {
         if (this.isEnabled()) {
-            if (var1.getVoxelShape() != null && var1.getBlockPos().getY() == this.field23843 - 1
-                    || var1.getBlockPos().getY() == this.field23843
-                    || var1.getBlockPos().getY() == this.field23843 + 1 && mc.world.getBlockState(var1.getBlockPos()).getBlock() instanceof SlabBlock
-                    || mc.world.getBlockState(var1.getBlockPos()).getBlock() instanceof Class3465
-                    || mc.world.getBlockState(var1.getBlockPos()).getBlock() instanceof StairsBlock
-                    || mc.world.getBlockState(var1.getBlockPos()).getBlock() instanceof Class3415
-                    || mc.world.getBlockState(var1.getBlockPos()).getBlock() instanceof Class3377) {
-                var1.setBoxelShape(null);
-            }
-        }
-    }
-
-    @EventTarget
-    private void method16682(EventPushBlock var1) {
-        if (this.isEnabled()) {
-            var1.setCancelled(true);
+            event.setCancelled(true);
         }
     }
 
     @Override
     public void onEnable() {
-        this.field23843 = (int) mc.player.getPosY();
-        this.field23844 = (double) this.field23843 - mc.player.getPosY();
+        currentYPosition = (int) mc.player.getPosY();
+        yOffset = (double) currentYPosition - mc.player.getPosY();
     }
 }
