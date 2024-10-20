2658ab3bf892babec8fbb69a53ab815330cfc962
coffee <185070888+ccfeeX@users.noreply.github.com>
2024-10-20 15:50:31 +0200

    Update NoViewReset.java

--- src/java/com/mentalfrostbyte/jello/module/impl/player/NoViewReset.java
+++ src/java/com/mentalfrostbyte/jello/module/impl/player/NoViewReset.java
@@ -12,16 +12,16 @@ public class NoViewReset extends Module {
     }
 
     @EventTarget
-    private void method16723(ReceivePacketEvent var1) {
+    private void RecievePacketEvent(ReceivePacketEvent event) {
         if (this.isEnabled()) {
             if (mc.player != null) {
                 if (mc.player.ticksExisted >= 10) {
-                    if (mc.player != null && var1.getPacket() instanceof SPlayerPositionLookPacket) {
-                        SPlayerPositionLookPacket var4 = (SPlayerPositionLookPacket) var1.getPacket();
-                        mc.player.prevRotationYaw = var4.yaw;
-                        mc.player.prevRotationPitch = var4.pitch;
-                        var4.yaw = mc.player.rotationYaw;
-                        var4.pitch = mc.player.rotationPitch;
+                    if (mc.player != null && event.getPacket() instanceof SPlayerPositionLookPacket) {
+                        SPlayerPositionLookPacket positionLookPacket = (SPlayerPositionLookPacket) event.getPacket();
+                        mc.player.prevRotationYaw = positionLookPacket.yaw;
+                        mc.player.prevRotationPitch = positionLookPacket.pitch;
+                        positionLookPacket.yaw = mc.player.rotationYaw;
+                        positionLookPacket.pitch = mc.player.rotationPitch;
                     }
                 }
             }
