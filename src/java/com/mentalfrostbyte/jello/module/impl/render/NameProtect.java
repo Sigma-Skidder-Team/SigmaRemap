0f9deeaba4a2533d4dd9a63ff721eb9c2bfd69fb
coffee <185070888+ccfeeX@users.noreply.github.com>
2024-10-20 16:10:14 +0200

    Remapped NameProtect.java

--- src/java/com/mentalfrostbyte/jello/module/impl/render/NameProtect.java
+++ src/java/com/mentalfrostbyte/jello/module/impl/render/NameProtect.java
@@ -20,74 +20,76 @@ public class NameProtect extends Module {
     }
 
     @EventTarget
-    public void method16883(TextReplaceEvent var1) {
+    public void TextReplaceEvent(TextReplaceEvent event) {
         if (this.isEnabled()) {
-            var1.setText(var1.setText().replaceAll(mc.getSession().getUsername(), this.getStringSettingValueByName("Username")));
+            event.setText(event.setText().replaceAll(mc.getSession().getUsername(), this.getStringSettingValueByName("Username")));
         }
     }
 
     @EventTarget
-    public void method16884(ReceivePacketEvent var1) {
+    public void onReceivePacket(ReceivePacketEvent receivePacketEvent) {
         if (this.isEnabled()) {
-            IPacket var4 = var1.getPacket();
-            if (var4 instanceof SUpdateScorePacket) {
-                SUpdateScorePacket var5 = (SUpdateScorePacket) var4;
-                if (var5.method17476() == Class2073.field13503) {
-                    String var6 = var5.method17473();
-                    if (var6.contains(mc.getSession().getUsername())) {
-                        var6 = var6.replaceAll(mc.getSession().getUsername(), this.getStringSettingValueByName("Username"));
-                        var1.method13899(new SUpdateScorePacket(var5.method17476(), var5.method17474(), var6, var5.method17475()));
+            IPacket packet = receivePacketEvent.getPacket();
+
+            if (packet instanceof SUpdateScorePacket) {
+                SUpdateScorePacket scorePacket = (SUpdateScorePacket) packet;
+                if (scorePacket.method17476() == Class2073.field13503) {
+                    String originalUsername = scorePacket.method17473();
+                    String replacementUsername = this.getStringSettingValueByName("Username");
+                    if (originalUsername.contains(mc.getSession().getUsername())) {
+                        originalUsername = originalUsername.replaceAll(mc.getSession().getUsername(), replacementUsername);
+                        receivePacketEvent.method13899(new SUpdateScorePacket(scorePacket.method17476(), scorePacket.method17474(), originalUsername, scorePacket.method17475()));
                     }
                 }
             }
 
-            if (var4 instanceof SChatPacket) {
-                SChatPacket var10 = (SChatPacket) var4;
+            if (packet instanceof SChatPacket) {
+                SChatPacket chatPacket = (SChatPacket) packet;
             }
 
-            if (var4 instanceof SPlayerListItemPacket) {
-                SPlayerListItemPacket var11 = (SPlayerListItemPacket) var4;
-                List var15 = var11.getEntries();
+            if (packet instanceof SPlayerListItemPacket) {
+                SPlayerListItemPacket playerListPacket = (SPlayerListItemPacket) packet;
+                List<SPlayerListItemPacket.AddPlayerData> playerEntries = playerListPacket.getEntries();
 
-                for (int var7 = 0; var7 < var15.size(); var7++) {
-                    SPlayerListItemPacket.AddPlayerData var8 = (SPlayerListItemPacket.AddPlayerData) var15.get(var7);
-                    if (var8.getDisplayName() != null) {
-                        String var9 = var8.getDisplayName().getString();
-                        if (var9.contains(mc.getSession().getUsername())) {
-                            var9 = var9.replaceAll(mc.getSession().getUsername(), this.getStringSettingValueByName("Username"));
-                            var8.displayName = new StringTextComponent(var9);
+                for (SPlayerListItemPacket.AddPlayerData playerData : playerEntries) {
+                    if (playerData.getDisplayName() != null) {
+                        String displayName = playerData.getDisplayName().getString();
+                        String replacementUsername = this.getStringSettingValueByName("Username");
+                        if (displayName.contains(mc.getSession().getUsername())) {
+                            displayName = displayName.replaceAll(mc.getSession().getUsername(), replacementUsername);
+                            playerData.displayName = new StringTextComponent(displayName);
                         }
                     }
                 }
 
-                var11.players = var15;
+                playerListPacket.players = playerEntries;
             }
 
-            if (var4 instanceof SUpdateBossInfoPacket) {
-                SUpdateBossInfoPacket var12 = (SUpdateBossInfoPacket) var4;
-                if (var12.getName() == null) {
+            if (packet instanceof SUpdateBossInfoPacket) {
+                SUpdateBossInfoPacket bossInfoPacket = (SUpdateBossInfoPacket) packet;
+                if (bossInfoPacket.getName() == null) {
                     return;
                 }
 
-                String var16 = var12.getName().getString();
-                if (var16.contains(mc.getSession().getUsername())) {
-                    var16 = var16.replaceAll(mc.getSession().getUsername(), this.getStringSettingValueByName("Username"));
-                    StringTextComponent var20 = new StringTextComponent(var16);
-                    var12.setName(var20);
+                String bossName = bossInfoPacket.getName().getString();
+                String replacementUsername = this.getStringSettingValueByName("Username");
+                if (bossName.contains(mc.getSession().getUsername())) {
+                    bossName = bossName.replaceAll(mc.getSession().getUsername(), replacementUsername);
+                    bossInfoPacket.setName(new StringTextComponent(bossName));
                 }
             }
 
-            if (var4 instanceof STitlePacket) {
-                STitlePacket var13 = (STitlePacket) var4;
-                if (var13.method17570() == null) {
+            if (packet instanceof STitlePacket) {
+                STitlePacket titlePacket = (STitlePacket) packet;
+                if (titlePacket.method17570() == null) {
                     return;
                 }
 
-                String var18 = var13.method17570().getString();
-                if (var18.contains(mc.getSession().getUsername())) {
-                    var18 = var18.replaceAll(mc.getSession().getUsername(), this.getStringSettingValueByName("Username"));
-                    StringTextComponent var21 = new StringTextComponent(var18);
-                    var1.method13899(new STitlePacket(var13.method17569(), var21, var13.method17571(), var13.method17572(), var13.method17573()));
+                String titleText = titlePacket.method17570().getString();
+                String replacementUsername = this.getStringSettingValueByName("Username");
+                if (titleText.contains(mc.getSession().getUsername())) {
+                    titleText = titleText.replaceAll(mc.getSession().getUsername(), replacementUsername);
+                    receivePacketEvent.method13899(new STitlePacket(titlePacket.method17569(), new StringTextComponent(titleText), titlePacket.method17571(), titlePacket.method17572(), titlePacket.method17573()));
                 }
             }
         }
