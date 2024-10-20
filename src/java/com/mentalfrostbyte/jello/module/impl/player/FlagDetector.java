a70038d1f2fec46455a4af6628edbcc4ceb1c665
coffee <185070888+ccfeeX@users.noreply.github.com>
2024-10-20 17:26:24 +0200

    Added FlagDetector.

---
+++ src/java/com/mentalfrostbyte/jello/module/impl/player/FlagDetector.java
@@ -0,0 +1,21 @@
+package com.mentalfrostbyte.jello.module.impl.player;
+
+import com.mentalfrostbyte.jello.Client;
+import com.mentalfrostbyte.jello.event.EventTarget;
+import com.mentalfrostbyte.jello.event.impl.ReceivePacketEvent;
+import com.mentalfrostbyte.jello.module.Module;
+import com.mentalfrostbyte.jello.module.ModuleCategory;
+import com.mentalfrostbyte.jello.notification.Notification;
+import net.minecraft.network.play.server.SPlayerPositionLookPacket;
+
+public class FlagDetector extends Module {
+    public FlagDetector() {
+        super(ModuleCategory.PLAYER,"FlagDetector", "Detects flags");
+    }
+    @EventTarget
+    public void RecievePacketEvent(ReceivePacketEvent event) {
+        if (event.getPacket() instanceof SPlayerPositionLookPacket && mc.player != null) {
+                Client.getInstance().getNotificationManager().send(new Notification("FlagDetector", "Detected Flag"));
+            }
+        }
+    }
