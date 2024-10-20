42a955997ea7f6225972265392f3ca80c7a265b3
coffee <185070888+ccfeeX@users.noreply.github.com>
2024-10-20 18:08:05 +0200

    Update ModuleManager.java

--- src/java/com/mentalfrostbyte/jello/module/ModuleManager.java
+++ src/java/com/mentalfrostbyte/jello/module/ModuleManager.java
@@ -76,7 +76,6 @@ public class ModuleManager {
         this.register(new HighJump());
         this.register(new ElytraFly());
         this.register(new Spider());
-      this.register(new FlagDetector());
         this.register(new KillAura());
         this.register(new AntiKnockback());
         this.register(new Criticals());
