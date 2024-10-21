711fe5e79002228e04623783c436297c78d4f613
Away <125528637+AwayXD@users.noreply.github.com>
2024-10-21 13:34:08 -0500

    Remap DVDSimulator.java

--- src/java/com/mentalfrostbyte/jello/module/impl/render/DVDSimulator.java
+++ src/java/com/mentalfrostbyte/jello/module/impl/render/DVDSimulator.java
@@ -14,67 +14,68 @@ import java.awt.Color;
 import java.io.IOException;
 
 public class DVDSimulator extends Module {
-    public Vector2f field23720 = new Vector2f(1.0F, 1.0F);
-    public float field23721;
-    public float field23722 = 0.0F;
-    public float field23723 = 1.0F;
-    public float field23724 = 1.0F;
-    public Class9108 field23725 = new Class9108(201, 90);
-    public int field23726 = 0;
+    public Vector2f dvdPosition = new Vector2f(1.0F, 1.0F);
+    public float dvdX;
+    public float dvdY = 0.0F;
+    public float xDirection = 1.0F;
+    public float yDirection = 1.0F;
+    public Class9108 dvdDimensions = new Class9108(201, 90);
+    public int dvdColor = 0;
 
     public DVDSimulator() {
         super(ModuleCategory.RENDER, "DVD Simulator", "wtf");
-        this.method16513();
+        this.changeColor();
     }
 
     @Override
-    
     public void onEnable() {
-        this.field23721 = (float) ((double) (mc.mainWindow.getWidth() - this.field23725.field41839) * Math.random());
-        this.field23722 = (float) ((double) (mc.mainWindow.getHeight() - this.field23725.field41840) * Math.random());
-        this.method16513();
+        this.dvdX = (float) ((double) (mc.mainWindow.getWidth() - this.dvdDimensions.field41839) * Math.random());
+        this.dvdY = (float) ((double) (mc.mainWindow.getHeight() - this.dvdDimensions.field41840) * Math.random());
+        this.changeColor();
     }
 
     @EventTarget
-    private void method16512(EventRender var1) throws IOException {
+    private void onRender(EventRender event) throws IOException {
         if (this.isEnabled() && mc.player != null && mc.world != null) {
-            int var4 = mc.mainWindow.getHeight();
-            int var5 = mc.mainWindow.getWidth();
-            float var6 = 2;
-            if (!(this.field23722 <= var6)) {
-                if (this.field23722 + (float) this.field23725.field41840 > (float) var4) {
-                    this.field23724 = -1.0F;
-                    this.method16513();
+            int windowHeight = mc.mainWindow.getHeight();
+            int windowWidth = mc.mainWindow.getWidth();
+            float speed = 2;
+
+            if (!(this.dvdY <= speed)) {
+                if (this.dvdY + (float) this.dvdDimensions.field41840 > (float) windowHeight) {
+                    this.yDirection = -1.0F;
+                    this.changeColor();
                 }
             } else {
-                this.field23724 = 1.0F;
-                this.method16513();
+                this.yDirection = 1.0F;
+                this.changeColor();
             }
 
-            if (!(this.field23721 <= var6)) {
-                if (this.field23721 + (float) this.field23725.field41839 > (float) var5) {
-                    this.field23723 = -1.0F;
-                    this.method16513();
+            if (!(this.dvdX <= speed)) {
+                if (this.dvdX + (float) this.dvdDimensions.field41839 > (float) windowWidth) {
+                    this.xDirection = -1.0F;
+                    this.changeColor();
                 }
             } else {
-                this.field23723 = 1.0F;
-                this.method16513();
+                this.xDirection = 1.0F;
+                this.changeColor();
             }
 
-            this.field23721 = this.field23721 + this.field23723 * var6;
-            this.field23722 = this.field23722 + this.field23724 * var6;
+            this.dvdX += this.xDirection * speed;
+            this.dvdY += this.yDirection * speed;
+
             RenderUtil.drawImage(
-                    this.field23721,
-                    this.field23722,
-                    (float) this.field23725.field41839,
-                    (float) this.field23725.field41840,
+                    this.dvdX,
+                    this.dvdY,
+                    (float) this.dvdDimensions.field41839,
+                    (float) this.dvdDimensions.field41840,
                     ResourceList.dvdPNG,
-                    MultiUtilities.applyAlpha(this.field23726, 0.8F)
+                    MultiUtilities.applyAlpha(this.dvdColor, 0.8F)
             );
         }
     }
 
-    private void method16513() {
-        this.field23726 = Color.getHSBColor((float) Math.random(), 0.6F, 1.0F).getRGB();
+    private void changeColor() {
+        this.dvdColor = Color.getHSBColor((float) Math.random(), 0.6F, 1.0F).getRGB();
     }
 }
