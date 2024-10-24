cb5193b8fa1fee6a5815544f82bb8c12894d6cc1
Mark <86848614+MarkGG8181@users.noreply.github.com>
2024-10-24 21:09:01 +0300

    remove obf leftovers

--- src/java/mapped/Class2188.java
+++ src/java/mapped/Class2188.java
@@ -3,6 +3,4 @@ package mapped;
 public enum Class2188 {
    field14308,
    field14309;
-
-   private static final Class2188[] field14310 = new Class2188[]{field14308, field14309};
 }
--- src/java/mapped/Class2311.java
+++ src/java/mapped/Class2311.java
@@ -3,6 +3,4 @@ package mapped;
 public enum Class2311 {
    field15840,
    field15841;
-
-   private static final Class2311[] field15842 = new Class2311[]{field15840, field15841};
 }
