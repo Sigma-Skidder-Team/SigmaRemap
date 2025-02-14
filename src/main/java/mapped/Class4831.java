573591f0d43bff3c73b226db91c937c569b9e202
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-02-14 23:13:18 +0200

    removing useless classes

--- src/main/java/mapped/Class4831.java
+++
@@ -1,4 +0,0 @@
-package mapped;
-
-public final class Class4831 extends Class4830 {
-}
--- src/main/java/mapped/Class6441.java
+++
@@ -1,9 +0,0 @@
-package mapped;
-
-import java.io.IOException;
-
-public interface Class6441 {
-   Sink method19554() throws IOException;
-
-   void method19553();
-}
